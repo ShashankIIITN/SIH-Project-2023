c5563750a91eb3ac44dc5cd42c614007f4d41a2b
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2023-09-21 15:07:53 +0530

    base

--- PG-Dissertation-System/frontend/src/App.js
+++ PG-Dissertation-System/frontend/src/App.js
@@ -1,10 +1,15 @@
+import Navbar from "./components/navbar/Navbar";
+import './components/navbar/Navbar.css'
 
 function App() {
+  const appdata = {
+    "name": "MyApp",
+  }
   return (
     <div className="App">
-      <h1>My name is khan</h1>
+      <Navbar appdata ={appdata} />
     </div>
-  );
+  )
 }
 
 export default App;
