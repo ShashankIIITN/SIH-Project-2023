11c2191530bb134d7d104048e105bb6ad2f17a8a
CoolSmit007 <agherasmit@gmail.com>
2023-09-23 16:46:29 +0530

    saerchbar update

--- PG-Dissertation-System/frontend/src/components/Searchbar/Searchbar.js
+++ PG-Dissertation-System/frontend/src/components/Searchbar/Searchbar.js
@@ -3,19 +3,26 @@ import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import TuneIcon from '@mui/icons-material/Tune';
-import { Grid,Paper } from '@mui/material';
+import { Collapse, Grid,Paper } from '@mui/material';
 import Advsearch from './Advsearch';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import React from "react";
+import "./searchbar.css"
 const Searchbar = () => {
-    return <Paper elevation={24}><Accordion><TextField fullWidth InputProps={{
-        startAdornment: <InputAdornment position="start"><SearchIcon/></InputAdornment>,
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+    return (<Collapse in={checked} orientation="horizontal" collapsedSize={55}><Paper elevation={24}><Accordion><TextField fullWidth InputProps={{
+        startAdornment: <InputAdornment position="start"><IconButton onClick={handleChange}><SearchIcon/></IconButton></InputAdornment>,
         endAdornment: <AccordionSummary><InputAdornment position="end"><IconButton><TuneIcon/></IconButton></InputAdornment></AccordionSummary>,
       }}></TextField>
       <AccordionDetails>
       <Advsearch/>
       </AccordionDetails>
-    </Accordion></Paper>;
+    </Accordion></Paper></Collapse>);
 };
 export default Searchbar;
