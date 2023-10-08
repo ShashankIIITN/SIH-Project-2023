977f4716de6892af551882fc21dec110f50011d0
CoolSmit007 <agherasmit@gmail.com>
2023-10-08 11:10:53 +0530

    searchbar fixed and looks nice

--- PG-Dissertation-System/frontend/src/components/Searchbar/Searchbar.js
+++ PG-Dissertation-System/frontend/src/components/Searchbar/Searchbar.js
@@ -30,7 +30,13 @@ const Searchbar = () => {
 
   const [back1, setback1] = React.useState('#1976d2');
 
-
+  const[rad,setrad] = React.useState(0);
+  const setrad1=()=>{
+    if(rad==10)
+    setrad((rad)=>0);
+  else
+  setrad((rad)=>10);
+  }
 
   const handleback1change = () => {
 
@@ -39,16 +45,23 @@ const Searchbar = () => {
     }
     else
       setback1((back1) => '#1976d2');
+    setrad1();
   }
   const handleChange = () => {
+    if(expand && checked)
+    toggleAcordion();
     setChecked((prev) => !prev);
   };
+  const [expand, setExpand] = React.useState(false);
+  const toggleAcordion = () => {
+    setExpand((prev) => !prev);
+  };
   return (<Collapse in={checked} timeout={500} orientation="horizontal" collapsedSize={54} onEntering={handleback1change} onExited={handleback1change}>
-    <Accordion fullwidth disableGutters={true} sx={{overflow:'visible'}}><TextField  onKeyDown={handleEnterKey} sx={{ background: back1, height: '3.3rem' }}
+    <Accordion fullwidth disableGutters={true} sx={{overflow:'visible'}} expanded={expand}><TextField  onKeyDown={handleEnterKey} sx={{ background: back1, height: '3.3rem', borderRadius:rad,}}
       fullWidth disabled={!checked} InputProps={{
         startAdornment: <InputAdornment position="start"><IconButton sx={{ background: back1 }} onClick={handleChange}><SearchIcon /></IconButton></InputAdornment>,
-        endAdornment: <AccordionSummary><InputAdornment position="end"><IconButton ><TuneIcon /></IconButton ></InputAdornment></AccordionSummary>,
-      }}></TextField>
+        endAdornment: <AccordionSummary><InputAdornment position="end"><IconButton onClick={toggleAcordion}><TuneIcon /></IconButton ></InputAdornment></AccordionSummary>,
+         sx: {borderRadius:10}}}></TextField>
       <AccordionDetails >
           <Advsearch  />
       </AccordionDetails>
